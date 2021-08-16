dec495074ceb5a29bdc1a2104ab3c663d305acce
vlad <vladislavkotx@gmail.com>
2021-08-16 22:57:34 +0300

    lab1

--- src/main/java/com/db/Start.java
+++ src/main/java/com/db/Start.java
@@ -1,30 +1,21 @@
 package com.db;
 
-import java.util.List;
+
+import homework.lab1.MySuperIntegerList;
+import homework.lab1.SuperIntegerList;
 
 /**
  * @author Evgeny Borisov
  */
 public class Start {
-
-
     public static void main(String[] args) throws InterruptedException {
-        for (String arg : args) {
-            System.out.println(arg);
-        }
-
-
-        List<Integer> integers = List.of(1, 2, 3);
-
-        if ((((Math.sqrt(integers.size()) == 5.0)))){
-            System.out.println("URA!");
-        }
-
-
-        for (Integer integer : integers) {
-            System.out.println(integer);
-        }
-
-
+        SuperIntegerList myArr = new MySuperIntegerList(new int[] {1, 3, 5});
+        myArr.printAll();
+        myArr.add(10);
+        myArr.printAll();
+        myArr.removeByIndex(1);
+        myArr.printAll();
+        myArr.removeByValue(5);
+        myArr.printAll();
     }
 }
--- src/main/java/homework/lab1/MySuperIntegerList.java
+++ src/main/java/homework/lab1/MySuperIntegerList.java
@@ -1,31 +1,51 @@
 package homework.lab1;
 
-/**
- * @author Evgeny Borisov
- */
-public class JekaSuperIntegerList implements SuperIntegerList {
+import java.util.Arrays;
+
+public class MySuperIntegerList implements SuperIntegerList {
+    private int [] myArr;
+
+
+    public MySuperIntegerList(int [] arr) {
+        myArr = arr;
+    }
+
     @Override
     public void add(int number) {
-
+        int [] newArr = new int[myArr.length + 1];
+        System.arraycopy(myArr, 0, newArr, 0, myArr.length);
+        newArr[myArr.length] = number;
+        myArr = newArr;
     }
 
     @Override
     public void removeByIndex(int index) {
-
+        int [] newArr = new int[myArr.length - 1];
+        if (index >= 0) System.arraycopy(myArr, 0, newArr, 0, index);
+        if (this.myArr.length - (index + 1) >= 0) {
+            System.arraycopy(myArr, index + 1, newArr, index, myArr.length - index - 1);
+        }
+        myArr = newArr;
     }
 
     @Override
     public void removeByValue(int value) {
-
+        for (int i = 0; i < myArr.length; i++) {
+            if (myArr[i] == value) {
+                this.removeByIndex(i--);
+            }
+        }
     }
 
     @Override
     public int get(int index) {
-        return 0;
+        return this.myArr[index];
     }
 
     @Override
     public void printAll() {
-
+        for (int i : myArr) {
+            System.out.print(i + " ");
+        }
     }
 }
